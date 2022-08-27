a0d656347aaafea6c3f55a1207a2e1bf7ed80457
anjali-tejasvi <anjalitejasvi501@gmail.com>
2022-08-27 21:03:45 +0530

    node js codes

--- prac/tut6/app.js
+++ prac/tut6/app.js
@@ -1,5 +1,5 @@
 const express =  require("express")
-
+const path = require("path")
 const app =  express();
 const port =  80;
 
@@ -14,14 +14,23 @@ app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
 
-//Our pug endpoint
-app.get('/demo', (req, res) => {
-    res.render('demo', { title: 'Hey', message: 'Hello there!' })
-  })
+//ENDPOINTS
+app.get('/', (req,res)=>{
+    const con = "This breed is very cute"
+    const params = {'title': 'Pug is the best breed', "content": con}
+    res.status(200).render('index.pug', params)
+})
 
-app.get("/", (req,res)=>{
-    res.send("<h3>This is my first Express App</h3>")
-});
+
+
+//Our pug endpoint
+// app.get('/demo', (req, res) => {
+//     res.render('demo', { title: 'Hey', message: 'Hello there!' })
+//   })
+
+// app.get("/", (req,res)=>{
+//     res.send("<h3>This is my first Express App</h3>")
+// });
 
 app.listen(port, ()=>{
     console.log(`The application is running on port ${port}`)
