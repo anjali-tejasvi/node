7fc88b4fd3d9517807a7dff07a9207d8a49ae2ec
anjali-tejasvi <anjalitejasvi501@gmail.com>
2022-08-28 13:22:07 +0530

    pug

--- prac/tut6/app.js
+++ prac/tut6/app.js
@@ -2,10 +2,12 @@ const express =  require("express")
 const path = require("path")
 const app =  express();
 const port =  80;
+const fs = require('fs')
+
 
 //For serving static files
 app.use('/static', express.static('static'))
-
+app.use(express.urlencoded())
 
 //Set the template engine as pug
 app.set('view engine', 'pug')
@@ -21,7 +23,19 @@ app.get('/', (req,res)=>{
     res.status(200).render('index.pug', params)
 })
 
+app.post('/',(req,res)=>{
 
+    name = req.body.name
+    age = req.body.age
+    gender = req.body.gender
+    address = req.body.address
+    more = req.body.more
+    let outputToWrite = `The name of the client is ${name}, ${age} years old, ${gender}, residing at ${address}.
+    More about him/her: ${more}`
+    fs.writeFileSync('output.txt', outputToWrite)
+    const params = {'message': 'Your form has been submitted succesfully'}
+    res.status(200).render('index.pug',params)
+})
 
 //Our pug endpoint
 // app.get('/demo', (req, res) => {
