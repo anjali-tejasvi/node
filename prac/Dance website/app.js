a7126b7fe97f9596c414a4fa35866bcbb3fbf1fd
anjali-tejasvi <anjalitejasvi501@gmail.com>
2022-09-10 09:44:48 +0530

    saving data in database

--- prac/Dance website/app.js
+++ prac/Dance website/app.js
@@ -2,6 +2,21 @@ const express = require('express');
 const  path = require("path")
 const app =  express();
 const port = 8000;
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/contactDance',{useNewUrlParser: true});
+const bodyparser = require('body-parser')
+
+//define mongoose schema
+var contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String,
+
+})
+
+var Contact = mongoose.model('Contact', contactSchema)
 
 //EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) //for serving static files
@@ -20,6 +35,16 @@ app.get('/',(req,res)=>{
 app.get('/contact',(req,res)=>{
     const params = {}
     res.status(200).render('contact.pug', params);
+})
+
+app.post('/contact',(req,res)=>{
+    var mydata = new Contact(req.body);
+    mydata.save().then(()=>{
+        res.send("This item has been saved to the database")
+    }).catch(()=>{
+        res.status(400).send("Item was not saved to the database")
+    });
+    //res.status(200).render('contact.pug');
 })
 
 //START THE SERVER
